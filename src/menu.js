68e5525bd338a9f1e47f88e9b2fa0e396327ee3e
Alex Buly <asbuly@icloud.com>
2024-06-17 20:27:04 -0400

    Adds remaining menu items

--- src/menu.js
+++ src/menu.js
@@ -18,10 +18,9 @@ const Menu = () => {
 
     const flavors = document.createElement("div");
     flavors.classList.add("ice-cream-flavors");
-    menuDiv.appendChild(flavors);
+    menu.appendChild(flavors);
 
     const flavorsTitle = document.createElement("h3");
-    flavorsTitle.classList.add("flavors-title");
     flavorsTitle.textContent = "Flavors";
     flavors.appendChild(flavorsTitle);
 
@@ -60,11 +59,6 @@ const Menu = () => {
     const br = "<br>";
     print(flavorsArray, br, flavorsList);
 
-    const cones = document.createElement("p");
-    flavors.appendChild(cones);
-    const conesArr = ["Single - $3.00", "Double - $5.00", "Triple - $6.50"];
-    print(conesArr, br, cones);
-
     const sorbetsTitle = document.createElement("h3");
     sorbetsTitle.textContent = "Sorbet";
     flavors.appendChild(sorbetsTitle);
@@ -73,6 +67,103 @@ const Menu = () => {
     flavors.appendChild(sorbets);
     const sorbetArr = ["Mango", "Raspberry", "Lemon", "Pineapple Coconut"];
     print(sorbetArr, br, sorbets);
+
+    const icecreamPrice = document.createElement("h3");
+    icecreamPrice.textContent = "Ice Cream and Sorbet Prices";
+    flavors.appendChild(icecreamPrice);
+    const cones = document.createElement("p");
+    flavors.appendChild(cones);
+    const conesArr = ["Single - $3.00", "Double - $5.00", "Triple - $6.50"];
+    print(conesArr, br, cones);
+
+    const milkshakes = document.createElement("div");
+    milkshakes.classList.add("milkshakes");
+    menu.appendChild(milkshakes);
+
+    const milkshakeTitle = document.createElement("h3");
+    milkshakeTitle.textContent = "Milkshakes";
+    milkshakes.appendChild(milkshakeTitle); 
+
+    const shakeList = document.createElement("p");
+    milkshakes.appendChild(shakeList);
+
+    const shakesArr = ["Classic Vanilla - $5.50", 
+                        "Chocolate Dream - $5.50",
+                        "Strawberry Bliss - $5.50",
+                        "Oreo Crumble - $6.00",
+                        "Peanut Butter Cup - $6.00",
+                        "Salted Caramel Pretzel - $6.50",
+                    ]
+    print(shakesArr, br, shakeList);
+
+    const icecreamSandwiches = document.createElement("div");
+    icecreamSandwiches.classList.add("ice-cream-sandwiches");
+    menu.appendChild(icecreamSandwiches);
+
+    const sandwichTitle = document.createElement("h3");
+    sandwichTitle.textContent = "Ice Cream Sandwiches - $5.00";
+    icecreamSandwiches.appendChild(sandwichTitle);
+
+    const sandwichList = document.createElement("p");
+    icecreamSandwiches.appendChild(sandwichList);
+
+    const sandwichArr = ["Chocolate Chip Cookie Sandwich", "Double Chocolate Sandwich", "Snickerdoodle Sandwich"];
+    print(sandwichArr, br, sandwichList);
+
+    const toppings = document.createElement("div");
+    toppings.classList.add("toppings");
+    menu.appendChild(toppings);
+
+    const toppingsTitle = document.createElement("h3");
+    toppingsTitle.textContent = "Toppings";
+    toppings.appendChild(toppingsTitle);
+
+    const toppingsList = document.createElement("p");
+    toppings.appendChild(toppingsList);
+
+    const toppingsArr = ["Sprinkles - $0.50",
+                         "Hot Fudge - $0.75",
+                         "Caramel Sauce - $0.75",
+                         "Whipped Cream - $0.50",
+                         "Nuts: $0.75",
+                         "Fresh Fruit - $1.00",
+                         "Cookie Crumbs - $0.50",
+                         "Gummy Bears - $0.75",
+                         "Marshmallows - $0.50"
+                        ]
+    print(toppingsArr, br, toppingsList);
+
+    const drinks = document.createElement("div");
+    drinks.classList.add("drinks");
+    menu.appendChild(drinks);
+
+    const drinksTitle = document.createElement("h3");
+    drinksTitle.textContent = "Drinks";
+    drinks.appendChild(drinksTitle);
+
+    const drinskList = document.createElement("p");
+    drinks.appendChild(drinskList);
+
+    const drinksArr = ["Cold Brew Coffee Float - $5.50",
+                       "Affogato - $4.50",
+                       "Root Beer Float - $5.00",
+                       "Italian Soda - $4.00",
+                    ]
+    print(drinksArr, br, drinskList);
+
+    const kidsMenu = document.createElement("div");
+    kidsMenu.classList.add("kids-menu");
+    menu.appendChild(kidsMenu);
+
+    const kidsTitle = document.createElement("h3");
+    kidsTitle.textContent = "Kids' Menu";
+    kidsMenu.appendChild(kidsTitle);
+
+    const kidsList = document.createElement("p");
+    kidsMenu.appendChild(kidsList);
+
+    const kidsArr = ["Mini Cones - $2.50", "Sprinkle Sundae - $4.00", "Ice Cream Cupcake - $3.50", ];
+    print(kidsArr, br, kidsList);
 }
 
 
